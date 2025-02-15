8631e66b37539634896d0c46ea656750afb76624
Nikita-P <nikitapoloynikov2013@yandex.com>
2025-02-15 18:50:08 +0300

    Создан проект

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Никита Полойников on 15.02.2025.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
